cb7898b7f79c063cedd1914cb0179259500741ec
JSpeir <speir.jack@gmail.com>
2021-10-13 15:00:41 +0100

    working sync

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-const form = document.getElementById("form");
-const log = document.getElementById("log");
-
-function logSubmit(event) {
-  log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
-  event.preventDefault();
+function loadXMLDoc(filename) {
+  if (window.ActiveXObject) {
+    xhttp = new ActiveXObject("Msxml2.XMLHTTP");
+  } else {
+    xhttp = new XMLHttpRequest();
+  }
+  xhttp.open("GET", filename, false);
+  try {
+    xhttp.responseType = "msxml-document";
+  } catch (err) {} // Helping IE11
+  xhttp.send("");
+  return xhttp.responseXML;
 }
 
-form.addEventListener("submit", logSubmit);
+function displayResult() {
+  xml = loadXMLDoc("cdcatalog.xml");
+  xsl = loadXMLDoc("cdcatalog.xsl");
+  // code for IE
+  if (window.ActiveXObject || xhttp.responseType == "msxml-document") {
+    ex = xml.transformNode(xsl);
+    document.getElementById("hello").innerHTML = ex;
+  }
+  // code for Chrome, Firefox, Opera, etc.
+  else if (document.implementation && document.implementation.createDocument) {
+    xsltProcessor = new XSLTProcessor();
+    xsltProcessor.importStylesheet(xsl);
+    resultDocument = xsltProcessor.transformToFragment(xml, document);
+    document.getElementById("log").appendChild(resultDocument);
+  }
+}
+
+displayResult();
